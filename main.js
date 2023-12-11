0da61f875573f4554b29085d2c23c178d82f0630
Camila Guimaraes <124820404+MilaGuimaraes@users.noreply.github.com>
2023-12-11 21:07:00 +0100

    Correcao
    Corrigindo o codigo para criar as classes filhas que herdarão atributos da classe mãe

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
-function Roupa(modelo, cor, tecido, tamanho, valor) {
+function Roupa(modelo, cor, tecido, valor) {
     this.modelo = modelo;
     this.cor = cor;
     this.tecido = tecido;
-    this.tamanho = tamanho;
     this.valor = valor
 }
 
-const roupaPeca1 = new Roupa('vestido', 'verde', 'viscose', 'pequeno', 19);
-const roupaPeca2 = new Roupa('calca', 'azul', 'jeans', 'grande', 50);
-const roupaPeca3 = new Roupa('blusa', 'branca', 'linho', 'medio', 30);
+function Vestido(modelo, cor, tecido, valor, tamanho) {
+    this.tamanho = tamanho;
 
-console.log(roupaPeca1);
-console.log(roupaPeca2);
-console.log(roupaPeca3);
+    Roupa.call(this, modelo, cor, tecido, valor);
+}
 
-console.log(roupaPeca1 instanceof Roupa);
+function Calca(modelo, cor, tecido, valor, cos) {
+    this.cos = cos;
+
+    Roupa.call(this, modelo, cor, tecido, valor);
+}
+ 
+function Camisa(modelo, cor, tecido, valor, manga) {
+    this.manga = manga;
+    
+    Roupa.call(this, modelo, cor, tecido, valor);
+}
+
+const vestido1 = new Roupa('vestido', 'verde', 'viscose', 19, 'curto');
+const calca1 = new Roupa('calca', 'azul', 'jeans', 50, 'alto');
+const camisa1 = new Roupa('camisa', 'laranja', 'linho', 30, 'com');
+const vestido2 = new Roupa('vestido', 'roxo', 'malha', 60, 'longo');
+const calca2 = new Roupa('calca', 'preta', 'moletom', 70, 'alto');
+const camisa2 = new Roupa('camisa', 'branca', 'algodao', 15, 'sem');
+
+
+console.log(vestido1);
+console.log(calca1);
+console.log(camisa1);
+console.log(vestido2);
+console.log(calca2);
+console.log(camisa2);
+
